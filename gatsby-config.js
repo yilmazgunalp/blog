0309e65cd1e0ee1753f29f312891f4952176a9b2
YILMAZ GUNDUZALP <yilmazgunalp@gmail.com>
2020-07-31 21:15:47 +1000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/content/posts/blog`,
+        path: `${__dirname}/content/posts/`,
       },
     },
     {
